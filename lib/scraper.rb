a547e9ed40f35c182c2d65263fe3efbcff44e090
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-01 02:29:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,22 +15,22 @@ class Scraper
       #   course.title = post.css("h2").text
       #   course.schedule = post.css(".date").text
       #   course.description = post.css("p").text
-    end
+    # end
   end
   
-  binding.pry
+
   
   
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
+  # def print_courses
+  #   self.make_courses
+  #   Course.all.each do |course|
+  #     if course.title && course.title != ""
+  #       puts "Title: #{course.title}"
+  #       puts "  Schedule: #{course.schedule}"
+  #       puts "  Description: #{course.description}"
+  #     end
+  #   end
+  # end
   
 end
 
