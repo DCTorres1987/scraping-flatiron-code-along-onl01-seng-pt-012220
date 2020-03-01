1f3046239229a35c428a7f28953344674b3bbc25
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-01 02:32:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,12 +16,14 @@ class Scraper
     self.get_page.css(".post")
   end
  
-      # doc.css(".post").each do |post|
-      #   course = Course.new
-      #   course.title = post.css("h2").text
-      #   course.schedule = post.css(".date").text
-      #   course.description = post.css("p").text
-    # end
+  def make_courses
+    
+      doc.css(".post").each do |post|
+        course = Course.new
+        course.title = post.css("h2").text
+        course.schedule = post.css(".date").text
+        course.description = post.css("p").text
+    end
   
   # def print_courses
   #   self.make_courses
