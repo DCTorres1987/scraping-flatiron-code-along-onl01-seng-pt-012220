eb5d60bbd6b81858707a808218cd9193ebd45261
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-03-01 02:30:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,6 +9,8 @@ class Scraper
  def get_page
  
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    
+    self.get_page.css(".post")
  
       # doc.css(".post").each do |post|
       #   course = Course.new
